128bd28f2539b3fd664646a3115f7c7a7efa23d7
pozys <pozys@mail.ru>
2021-06-09 15:34:52 +0300

    changed ip address

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-     uvicorn.run('main:app', host="192.168.56.104", port=8080, reload=True)
+     uvicorn.run('main:app', host="0.0.0.0", port=8080, reload=True)
