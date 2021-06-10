3e0b5d5a343cf324d9c19a9115ed47f0a92852ed
pozys <pozys@mail.ru>
2021-06-10 23:04:31 +0300

    test

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ server = os.environ.get('MARIA_DB_ADDR')
 if not server:
     server = '192.168.120.96'
 
-engine = create_engine(f"mariadb+pymysql://root:root@{server}:3306/error_author_advisor")
+engine = create_engine(f"mariadb+pymysql://admin:admin@{server}:3306/error_author_advisor")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
