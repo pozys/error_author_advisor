bc7134df97f67acefc8b709c6f220ee732a6ca5e
pozys <pozys@mail.ru>
2021-06-15 12:38:35 +0300

    added docker-entrypoint.sh

--- database.py
+++ database.py
@@ -5,23 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from app_settings import get_settings as settings
 
 server = settings('MARIA_DB_ADDR')
-
 user = settings('MARIADB_USER')
-if not user:
-    print('MARIADB_USER')
-    # raise errors.EnvError()
-
 psw = settings('MARIADB_PASSWORD')
-if not psw:
-    print('MARIADB_PASSWORD')
-    # raise errors.EnvError('MARIADB_PASSWORD')
-
 database = settings('MARIADB_DATABASE')
-if not database:
-    print('MARIADB_DATABASE')
-    # raise errors.EnvError('MARIADB_DATABASE')
 
-print('user', user, 'psw', psw, 'server', server, 'database', database)
 engine = create_engine(f"mariadb+pymysql://{user}:{psw}@{server}:3306/{database}")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
---
+++ docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ./error_author_advisor
+git pull $REPOSITORY_ADDR
+pyton3 server.py
